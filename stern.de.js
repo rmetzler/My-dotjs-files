a8127d5fe1d1be887430337375ff16b6979ad135
Richard Metzler <richard@eaglefeed.me>
2012-04-29 23:57:28 +0200

    remove ads on stern.de

--- stern.de.js
+++ stern.de.js
@@ -1,1 +1,1 @@
-jQuery('.ad_div_rectangle, .ad_div_halfpage, .ad_div_banner, .ad_div_skyscraper').remove()
+jQuery('.ad_div_rectangle, .ad_div_halfpage, .ad_div_banner, .ad_div_skyscraper, .ad_div_google').remove()
