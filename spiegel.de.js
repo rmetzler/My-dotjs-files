6ee41c1747d28a15760c7ab856b80f2cf090ec9f
Richard Metzler <richard.metzler@me.com>
2015-10-22 18:26:40 +0200

    old changes, untested

--- spiegel.de.js
+++ spiegel.de.js
@@ -1,1 +1,2 @@
-jQuery('#qcTop1, #spColumnAd, .spCommercial, .spMInline, .spMagazinkastenAd').remove()
+jQuery('#qcTop1, #spColumnAd, .spCommercial, .spMInline, .spMagazinkastenAd, .adform-adbox').remove()
+jQuery('.adform-adbox').remove()
